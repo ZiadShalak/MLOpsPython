1fb4a6e4d2b9c13c2e756e322409e6549ac9c0ce
Ziad <43904399+ZiadShalak@users.noreply.github.com>
2023-02-13 15:33:37 +0100

    Update demo_flake8.py

--- demo_flake8.py
+++ demo_flake8.py
@@ -1,5 +1,6 @@
-print("hellor world hellor world hellor world")
 from pathlib import path
 
+print("hellor world hellor world hellor world")
+
 package_dir = Path(__file__).parent.absolute()
-print(pacakge_dir)
+print(package_dir)
